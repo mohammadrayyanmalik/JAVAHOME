4ac6f0924849b961ab2d9dc1ef85c081b19f9be7
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-08-28 19:52:40 +0530

    println

---
+++ println.java
@@ -0,0 +1,14 @@
+class println
+{
+	public static void main (String [] args)
+	{
+		
+		System.out.println("Mohammad ");
+		System.out.println("Rayyan ");
+		System.out.println("malik");
+		System.out.println("mohammad rayyan malik");
+		System.out.println();
+		System.out.print();
+	}
+	
+}
