9bade439d789475ffae665e5a99c0bfeb06633e9
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-08-28 20:08:26 +0530

    datatype

---
+++ datatype.java
@@ -0,0 +1,34 @@
+class datatype 
+{
+	public static void main(String [] args)
+	{
+		// byte range -128 to 127
+		byte Rayyan=10;
+		System.out.println("this is the byte datatype "+Rayyan);
+		System.out.println("Rayyan="+Rayyan);
+		
+		
+		short a=10000;
+		System.out.println("the value of a is "+a+" is more than byte");
+	}
+	
+}
+
+
+
+
+
+
+// variable is a container which store information.
+// eg: x=3, y=6
+
+
+// to declarevariable syntex in java 
+// datatype variable_name;(when we don't want to declare any value)
+// datatype variable_name=value;(when we want to declare any value)
+
+
+// when we need to call 6 or 3 we can say x or y 
+// in variable we can store otherthan integer/number but in java we should declare also datatype.
+// float : we used float when number need in points but only 6 to 7 digit after pointsand more than 7 digit we used double.
+// 
