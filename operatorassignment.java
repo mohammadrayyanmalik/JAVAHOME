f6aa925404980c96b4840d7f6a1b9865ae948c8a
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-08-29 20:26:28 +0530

    assign operator

---
+++ operatorassignment.java
@@ -0,0 +1,57 @@
+class operatorassignment
+{
+	public static void main(String[]args)
+	{
+		int ram=100;
+		int shyam=70;
+		
+		ram+=(shyam-ram);
+		System.out.println(ram);
+		// 70
+		
+		
+		
+		float amit=100;
+		float wholeclass=10;
+		amit/=wholeclass;
+		System.out.println(amit);
+		System.out.println(wholeclass);
+		// 10
+		
+		int x=2;
+		int y=50;
+		y+=y;
+		// what is value of x.
+		System.out.println(y);
+		// 100
+		
+		
+		int a=50;
+		int b=2;
+		a*=(a/b);
+		System.out.println(a);
+		// 1250
+		
+		
+		
+		int e=98;
+		int f=9;
+		e%=f;
+		System.out.println(e);
+		//8
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
