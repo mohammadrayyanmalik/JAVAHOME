07c931fcd106dfb80c88acd519b2c60f29615b9a
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-08-29 15:41:02 +0530

    operator and its test

---
+++ areaoftriangle.java
@@ -0,0 +1,11 @@
+class areaoftriangle{
+	public static void main(String[] args)
+	{
+	float base=10f;
+	float height=20f;
+	System.out.println("areaoftriangle is "+(base*height/2));
+	
+	}
+	
+	
+}
---
+++ operator.java
@@ -0,0 +1,29 @@
+class operator 
+{
+	
+	public static void main(String[]args)
+	{
+		int a=1;
+		int b=10;
+		//	when we doing addition we should put the whole no together in () otherwise it consider the string while we want output integer the eg. is 
+										// + is operator and a ya b and is operand
+		System.out.println("rayyan walking speed is "+(a+b));
+		
+		
+		
+		// for substraction used
+		System.out.println(a-b);
+		
+		// for multiplication used
+		System.out.println(b*a+1-10);
+		
+		
+		// for division used /
+		System.out.println(b/a);
+		
+		// modulus used %
+		System.out.println(b%a);
+		
+	}
+	
+}
