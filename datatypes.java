5a0ae4ea7597e31a0d1d92a043939e6bb28303e5
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-08-29 15:07:53 +0530

    datatypes

---
+++ datatypes.java
@@ -0,0 +1,41 @@
+class datatypes 
+{
+	public static void main (String []args)
+	{
+		byte a=10;
+		System.out.println("the value of a+b is " +a);
+		
+		int c=100;
+		System.out.println(c);
+		
+		short ab=180;
+		System.out.println(ab);
+		
+		
+		// long datatype give error because default value is int so we should mentioned in last of no is small L;
+		
+		long ac=1862614789432l;
+		System.out.println(ac);
+		
+		
+		// float is used for points
+		// float datatype give error because default value is double so we should mentioned in last of no is f;
+		float d=299.92f;
+		System.out.println(d);
+		
+		
+		double ad=424.54365647;
+		System.out.println(ad);
+		
+		boolean  e=false;// we can pass false or true
+		System.out.println(e);
+		
+		char ae='a';
+		System.out.println(ae);
+		
+		
+		
+		
+	}
+	
+}
