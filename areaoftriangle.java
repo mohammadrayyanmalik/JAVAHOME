ad8824f82a60a221acab736983c81b801d05af08
Mohammad Rayyan <mrrayyan1801@gmail.com>
2024-09-01 19:30:26 +0530

    Update areaoftriangleandrectangle.java

--- areaoftriangle.java
+++ areaoftriangle.java
@@ -4,6 +4,16 @@ class areaoftriangle{
 	float base=10f;
 	float height=20f;
 	System.out.println("areaoftriangle is "+(base*height/2));
+
+
+		// Areaof rectangle
+		//rectangle formula is lenght*bredth
+		int l=10;
+		int b=20;
+		System.out.println("Area of ractangle is "+(l*b));
+		
+		
+		
 	
 	}
 	
