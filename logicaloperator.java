e9fa912f354195cac36a55d3006e47e380cad97f
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-09-02 19:03:28 +0530

    bitwise

---
+++ logicaloperator.java
@@ -0,0 +1,26 @@
+class logicaloperator
+{
+	public static void main(String[]args)
+	{	
+	
+		int Rayyan=10;
+		int Asrar=12;
+		
+		// and (&&) operator
+		System.out.println(Rayyan>Asrar && Asrar!=Rayyan);
+		System.out.println(Rayyan<Asrar && Asrar!=Rayyan);
+		
+		// or (||) operator
+		System.out.println(Rayyan>1000  ||   Asrar==12);
+		
+		
+		// ! NOt operator
+		System.out.println(!(Rayyan>Asrar && Asrar!=Rayyan));
+		
+		
+		
+		
+
+
+		}
+}
