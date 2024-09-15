7507fc3db04ee27196c72d4ffd3e480ab9df1661
Mohammad Rayyan <mrrayyan1801@gmail.com>
2024-09-15 20:26:39 +0530

    Create do_while.java

---
+++ do_while.java
@@ -0,0 +1,26 @@
+// pahle kam phir codition check karne ke liye hum do_while loops ka use karte hain
+// syntex of do_ while loops
+/* do {
+}while (condition);
+
+*/
+// kisi chis ko reorder ya rebook karne ke liye hum do whileloops ka use karenge
+
+
+
+import java.util.*;
+class do_while_loops
+{
+	public static void main(String[]args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int n;
+	do
+	{
+		System.out.println("Hello");
+		n=sc.nextInt();
+	}while(n<=10);
+	
+	
+	}
+}
